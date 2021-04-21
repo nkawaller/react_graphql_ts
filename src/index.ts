155dcae6e9a1bb350a8ce22cc960b3d41460abad
Nathan Kawaller <kawaller@gmail.com>
2021-04-21 08:51:23 -0400

    orm config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello, World!')
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [],
+    dbName: "react-gql",
+    user: "postgres",
+    password: "postgres",
+    debug: !__prod__,
+  });
+};
+
+main();
