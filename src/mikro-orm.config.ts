16c3936d8d47340f18d3328a5bf639a07af3693a
Nathan Kawaller <kawaller@gmail.com>
2021-04-21 09:31:25 -0400

    first migration

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,8 +1,13 @@
 import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from '@mikro-orm/core'
+import path from 'path';
 
 export default {
+    migrations: {
+        path: path.join(__dirname, './migrations'),
+        pattern: /^[\w-]+\d+\.[tj]s$/
+    },
   entities: [Post],
   dbName: "react-gql",
   user: "postgres",
