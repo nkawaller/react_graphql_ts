9502d8a6f7cd2124608d24de2e23d3be5021402a
Nathan Kawaller <kawaller@gmail.com>
2021-04-22 00:41:57 -0400

    get all posts

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,17 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field(() => Int)
+  @PrimaryKey()
+  id!: number;
 
-    @PrimaryKey()
-    id!: number;
+  @Field(() => String)
+  @Property({ type: "date" })
+  createdAt = new Date();
 
-    @Property({type: 'date'})
-    createdAt = new Date();
+  @Field(() => String)
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt = new Date();
 
-    @Property({ type: 'date', onUpdate: () => new Date() })
-    updatedAt = new Date();
-
-  @Property({type: 'text'})
+  @Field()
+  @Property({ type: "text" })
   title!: string;
 }
