6c898b003b58b2dd3a0df00e51808a6203df7e34
Nathan Kawaller <kawaller@gmail.com>
2021-04-22 09:58:23 -0400

    delete post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -37,10 +37,19 @@ export class PostResolver {
     if (!post) {
       return null;
     }
-    if (typeof title !== 'undefined') {
+    if (typeof title !== "undefined") {
       post.title = title;
-      await em.persistAndFlush(post)
+      await em.persistAndFlush(post);
     }
     return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id });
+    return true;
   }
 }
