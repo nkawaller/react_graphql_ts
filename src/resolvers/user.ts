534e808efdc9f2298e7350b771558af8b8249a55
Nathan Kawaller <kawaller@gmail.com>
2021-04-23 00:55:25 -0400

    me query to return current user

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -5,6 +5,7 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver,
 } from "type-graphql";
 import { User } from "../entities/User";
@@ -38,10 +39,21 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em}: MyContext) {
+    if (!req.session.userId) {
+
+      return null
+    } 
+    const user = await em.findOne(User, {id: req.session.userId}) 
+    return user
+
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -85,6 +97,11 @@ export class UserResolver {
         };
       }
     }
+
+    // store user id session
+    // this will set a cookie on the user
+    // logs in the user after registering 
+    req.session.userId = user.id;
     return { user };
   }
 
