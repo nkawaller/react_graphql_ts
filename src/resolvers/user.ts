73971ca03501e3608e932fc21283940f09a45211
Nathan Kawaller <kawaller@gmail.com>
2021-04-22 20:36:16 -0400

    register mutation

---
+++ src/resolvers/user.ts
@@ -0,0 +1,36 @@
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  Resolver,
+} from "type-graphql";
+import { User } from "../entities/User";
+import { MyContext } from "../types";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => User)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ) {
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    await em.persistAndFlush(user);
+    return user;
+  }
+}
